f3ef1de37a03aad053ae60718ea8855fc03bb9e8
kinkeadian@outlook.com <kinkeadian@outlook.com>
2022-11-05 14:10:40 -0700

    WarPrep and ColoredItems

---
+++ PartTwoOOP/ColoredItems/ColoredItems/Program.cs
@@ -0,0 +1,29 @@
+﻿ColoredItem<Sword> sword = new ColoredItem<Sword>(new Sword(), ConsoleColor.DarkCyan);
+ColoredItem<Bow> bow = new ColoredItem<Bow>(new Bow(), ConsoleColor.Magenta);
+ColoredItem<Axe> axe = new ColoredItem<Axe>(new Axe(), ConsoleColor.Red);
+
+sword.Display();
+bow.Display();
+axe.Display();
+Console.ForegroundColor = ConsoleColor.Gray;
+
+public class Sword { }
+public class Bow { }
+public class Axe { }
+
+public class ColoredItem<T>
+{
+    public T Weapon { get; }
+    public ConsoleColor Color { get; }
+
+    public ColoredItem(T weapon, ConsoleColor color)
+    {
+        Weapon = weapon;
+        Color = color;
+    }
+    public void Display()
+    {
+        Console.ForegroundColor = Color;
+        Console.WriteLine(Weapon);
+    }
+} 
---
+++ PartTwoOOP/WarPreparations/WarPreparations/Program.cs
@@ -0,0 +1,11 @@
+﻿Sword basic = new Sword(MaterialType.Iron, GemType.None, 50, 10);
+Sword enhanced = new Sword(MaterialType.Binarium, GemType.Sapphire, 70, 18);
+Sword wonky = new Sword(MaterialType.Wood, GemType.Bitstone, 40, 20);
+
+Console.WriteLine(basic);
+Console.WriteLine(enhanced);
+Console.WriteLine(wonky);
+
+public record Sword(MaterialType materialType, GemType gemType, int length, int crossguardWidth); 
+public enum MaterialType { Wood, Bronze, Iron, Steel, Binarium }
+public enum GemType { Emerald, Amber, Sapphire, Diamond, Bitstone, None}
